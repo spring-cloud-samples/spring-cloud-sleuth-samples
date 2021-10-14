b0dc04c56670b599132c81e25d567064cc509d58
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-10-14 12:13:28 +0200

    It works

--- stream-reactive-producer/src/main/java/com/example/sleuthsamples/StreamReactiveProducerApplication.java
+++ stream-reactive-producer/src/main/java/com/example/sleuthsamples/StreamReactiveProducerApplication.java
@@ -1,23 +1,25 @@
 package com.example.sleuthsamples;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Flux;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.function.context.PollableBean;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class StreamReactiveProducerApplication implements CommandLineRunner {
@@ -33,13 +35,25 @@ public class StreamReactiveProducerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		log.warn("Remember about setting <spring.cloud.sleuth.integration.enabled=true> property for Stream Reactive and Sleuth to work");
 		this.streamBridgeService.call();
 	}
 
+	// Function<Mono<?>, Mono<?>> and Supplier<Mono<?>> and Consumer<Mono<?>> are not supported in Stream
+
 	@Bean
-	Function<Flux<String>, Flux<String>> channel(Tracer tracer) {
-		return s -> s.doOnNext(i -> log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from producer", tracer.currentSpan().context().traceId()));
+	Function<Flux<Message<String>>, Flux<Message<String>>> tracingFunction(Tracer tracer) {
+		return s -> s.doOnNext(i -> log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from producer function flux", tracer.currentSpan().context().traceId()));
+	}
+
+	// @PollableBean
+	Supplier<Flux<String>> supplier(Tracer tracer) {
+		return () -> Flux.just("HELLO")
+			.doOnNext(s -> log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from producer supplier mono", tracer.currentSpan().context().traceId()));
+	}
+
+	// @PollableBean
+	Supplier<Flux<String>> stringSupplier(Tracer tracer) {
+		return () -> Flux.just("a", "b").doOnNext(s -> log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from producer supplier flux", tracer.currentSpan().context().traceId()));
 	}
 }
 
@@ -58,9 +72,9 @@ class StreamBridgeService {
 
 	void call() {
 		Span span = this.tracer.nextSpan();
-		try (Tracer.SpanInScope ws = this.tracer.withSpan(span.start())) {
+		try (Tracer.SpanInScope ws = this.tracer.withSpan(span.name("stream-bridge-service").start())) {
 			log.info("Hello from stream bridge - trace <{}>", this.tracer.currentSpan().context().traceId());
-			this.streamBridge.send("channel-in-0", "HELLO");
+			this.streamBridge.send("tracingFunction-in-0", "HELLO");
 		} finally {
 			span.end();
 		}
