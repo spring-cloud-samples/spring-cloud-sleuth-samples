bee89fac6655d283e4450eaee0da45bcaf389e1b
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-05-20 15:35:37 -0700

    Fix unused import

--- data/src/main/java/com/example/sleuthsamples/DataApplication.java
+++ data/src/main/java/com/example/sleuthsamples/DataApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataAccessException;
-import org.springframework.transaction.TransactionException;
 
 @SpringBootApplication
 public class DataApplication {
